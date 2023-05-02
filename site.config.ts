455795bc283aa5e2fdcf25891023fa69af503e80
JaveleyQAQ <132129852+JaveleyQAQ@users.noreply.github.com>
2023-05-02 21:19:18 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,8 +26,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: '',
-  defaultPageCover: '',
+  defaultPageIcon: 'https://blog-4kysoe0e5-javeleyqaq.vercel.app/page-icon.jpg',
+  defaultPageCover: 'https://blog-4kysoe0e5-javeleyqaq.vercel.app/page-icon.jpg',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
