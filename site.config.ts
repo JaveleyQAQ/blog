5812e793bd177f4b59a992d3090a1ea2e7e8751a
JaveleyQAQ <132129852+JaveleyQAQ@users.noreply.github.com>
2023-05-02 21:43:51 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://javeley.vercel.app/page-icon.jpg',
+  defaultPageIcon: 'https://javeley.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fc7a88bbe-e3cd-44d2-8ad7-6e6f39fa8b46%2Fpage-icon.jpg?id=a7b2dca1-78e3-4ae5-bd7f-ecd4284fdb25&table=block&spaceId=a60c8573-7dc4-41f6-9d1e-aeeb67d670d8&width=640&userId=&cache=v2',
   defaultPageCover: 'https://javeley.vercel.app/page-cover.jpg',
   defaultPageCoverPosition: 0.5,
 
