5c09060dbe778d31c28d42f86e7626427cc6248a
Javeley <javeley@outlook.com>
2023-05-03 13:25:50 +0800

    123

--- site.config.ts
+++ site.config.ts
@@ -1,69 +1,69 @@
-import { siteConfig } from './lib/site-config'
+  import { siteConfig } from './lib/site-config'
 
-export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '95c0ae7dd8424c2f9472749ad4eb6649',
+  export default siteConfig({
+    // the site's root Notion page (required)
+    rootNotionPageId: '95c0ae7dd8424c2f9472749ad4eb6649',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-  //rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
-  rootNotionSpaceId: 'null',
+    // if you want to restrict pages to a single notion workspace (optional)
+    // (this should be a Notion ID; see the docs for how to extract this)
+    //rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
+    rootNotionSpaceId: 'null',
 
-  // basic site info (required)
-  name: 'Javeley Blog',
-  domain: 'https://javeley.notion.site/Javeley-',
-  author: 'Javeley',
+    // basic site info (required)
+    name: 'Javeley Blog',
+    domain: 'https://javeley.notion.site/',
+    author: 'Javeley',
 
-  // open graph metadata (optional)
-  description: '兴趣是最好的老师👩',
+    // open graph metadata (optional)
+    description: '兴趣是最好的老师👩',
 
-  // social usernames (optional)
-  twitter: 'JaveleyQAQ',
-  github: 'JaveleyQAQ',
-  zhihu: 'Javeley',
-  //linkedin: 'JaveleyQAQ',
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+    // social usernames (optional)
+    twitter: 'JaveleyQAQ',
+    github: 'JaveleyQAQ',
+    zhihu: 'Javeley',
+    //linkedin: 'JaveleyQAQ',
+    // newsletter: '#', // optional newsletter URL
+    // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://raw.githubusercontent.com/JaveleyQAQ/blog/transitive-bullshit/public/page-icon.jpg',
-  defaultPageCover: 'https://transitivebullsh.it/page-cover.jpg',
-  defaultPageCoverPosition: 0.5,
+    // default notion icon and cover images for site-wide consistency (optional)
+    // page-specific values will override these site-wide defaults
+    defaultPageIcon: 'https://raw.githubusercontent.com/JaveleyQAQ/blog/transitive-bullshit/public/page-icon.jpg',
+    defaultPageCover: 'https://transitivebullsh.it/page-cover.jpg',
+    defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+    // whether or not to enable support for LQIP preview images (optional)
+    isPreviewImageSupportEnabled: true,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
+    // whether or not redis is enabled for caching generated preview images (optional)
+    // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+    // environment variables. see the readme for more info
+    isRedisEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+    // map of notion page IDs to URL paths (optional)
+    // any pages defined here will override their default URL paths
+    // example:
+    //
+    // pageUrlOverrides: {
+    //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+    //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+    // }
+    pageUrlOverrides: null,
 
-  pageUrlAdditions: {
-    '/blog': '95c0ae7dd8424c2f9472749ad4eb6649'
-  },
-
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'About',
-      pageId: '306e48b4243247ae8fb2369d8a9fc3a4'
+    pageUrlAdditions: {
+      '/blog': '95c0ae7dd8424c2f9472749ad4eb6649'
     },
-    {
-      title: 'Contact',
-      pageId: '7a34404d79a142d494cfae570c958645'
-    }
-  ]
-})
+
+    // whether to use the default notion navigation style or a custom one with links to
+    // important pages
+    navigationStyle: 'custom',
+    navigationLinks: [
+      {
+        title: 'About',
+        pageId: '306e48b4243247ae8fb2369d8a9fc3a4'
+      },
+      {
+        title: 'Contact',
+        pageId: '7a34404d79a142d494cfae570c958645'
+      }
+    ]
+  })
